87e335d987e062433376283249b0482dabc9f639
FlyinZebra <33535523+ChinmayMoghe@users.noreply.github.com>
2026-05-06 17:49:14 +0530

    Add: house-robber - submission-4

---
+++ Data Structures & Algorithms/house-robber/submission-4.js
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    rob(nums) {
+      const cache = new Map();
+      function dp(i) {
+        if(i>=nums.length) {
+            return 0;
+        }
+        let result;
+        if(cache.has(i)) {
+            result = cache.get(i);
+        } else {
+         result = Math.max(nums[i] + dp(i+2),dp(i+1));
+         cache.set(i,result);
+        }
+        return result;
+      }
+      return dp(0);
+    }
+}
