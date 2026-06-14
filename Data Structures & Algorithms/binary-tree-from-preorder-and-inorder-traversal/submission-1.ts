c9cbd0da2416fb6feb09c527079871a3318d161b
FlyinZebra <33535523+ChinmayMoghe@users.noreply.github.com>
2026-06-14 21:08:37 +0530

    Add: binary-tree-from-preorder-and-inorder-traversal - submission-1

---
+++ Data Structures & Algorithms/binary-tree-from-preorder-and-inorder-traversal/submission-1.ts
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {number[]} preorder
+     * @param {number[]} inorder
+     * @return {TreeNode}
+     */
+    buildTree(preorder: number[], inorder: number[]): TreeNode {
+        // keep it shared since we need to make sure the tree is 
+        // built sequentially with pre order array 
+        let pre_index = 0;
+
+        let inorderLookup = new Map(Object.entries(inorder).map(([k,v])=>[v,Number(k)]));
+
+        function buildTree(start:number, end:number): TreeNode {
+            if(start > end) {
+                return null;
+            }
+            let root_val = preorder[pre_index];
+            let root = new TreeNode(root_val);
+            pre_index += 1; // move to next root 
+            
+            // O(n) look up here - can be optimized to be a hashmap look up
+            let inorder_idx = inorderLookup.get(root_val);
+            // we send in the left subtree slice to build left subtree
+            root.left = buildTree(start, inorder_idx - 1);
+            // we send in the right subtree slice to build right subtree 
+            root.right = buildTree(inorder_idx+1, end);
+            // finally return root element
+            return root;
+        }
+
+        return buildTree(0, inorder.length - 1);
+    }
+}
